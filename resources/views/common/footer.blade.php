2079138a6e18327607efe229a7187d6dce6d0956
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:52:31 -0400

    Create footer.blade.php

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,50 @@
+<footer class="site-footer" style="display: block; height: 691px;">
+    <!-- footer top part -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget widget_about">
+                        <div class="logo-footer logo-white"><img src="https://autocare.dexignlab.com/xhtml/images/logo.png" alt=""></div>
+                        <p><strong>Auto Care</strong> ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore agna aliquam erat . wisi enim ad minim veniam, quis tation. sit amet, consec tetuer.ipsum dolor sit amet, consectetuer.</p>
+                        <ul class="dlab-social-icon dez-border">
+                            <li><a class="fab fa-facebook-f" href="https://www.facebook.com/" target="blank"></a></li>
+                            <li><a class="fab fa-twitter" href="https://twitter.com/" target="blank"></a></li>
+                            <li><a class="fab fa-linkedin-in" href="https://www.linkedin.com/" target="blank"></a></li>
+                            <li><a class="fab fa-facebook-f" href="https://www.facebook.com/" target="blank"></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget widget_services">
+                        <h4 class="m-b15 text-uppercase">{{__('Our services')}}</h4>
+                        <div class="dlab-separator-outer m-b10">
+                            <div class="dlab-separator bg-white style-skew"></div>
+                        </div>
+                        <ul>
+                            <li><a href="engine-diagnostics.html">Engine Diagnostics</a></li>
+                            <li><a href="lube-oil-and-filters.html">Lube, Oil and Filters</a></li>
+                            <li><a href="belts-and-hoses.html">Belts and Hoses</a></li>
+                            <li><a href="air-conditioning.html">Air Conditioning</a></li>
+                            <li><a href="brake-repair.html">Brake Repair</a></li>
+                            <li><a href="tire-and-wheel-services.html">Tire And Wheel</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget widget_getintuch">
+                        <h4 class="m-b15 text-uppercase">{{__('Contact us')}}</h4>
+                        <div class="dlab-separator-outer m-b10">
+                            <div class="dlab-separator bg-white style-skew"></div>
+                        </div>
+                        <ul>
+                            <li><i class="ti-location-pin"></i><strong>{{__('Address')}}</strong> demo address #8901 Marmora Road Chi Minh City, Vietnam </li>
+                            <li><i class="ti-mobile"></i><strong>{{__('Telephone')}}</strong>0800-123456 (24/7 Support Line)</li>
+                            <li><i class="ti-email"></i><strong>email</strong>info@demo.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
