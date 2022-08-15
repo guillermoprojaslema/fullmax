6a28dbdd066078dc35b614e3be42d9315a1a9566
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:27:09 -0400

    Create ContactController.php

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contactmessage;
+use App\Http\Requests\ContactRequest;
+use App\Mail\Contact;
+use Illuminate\Support\Facades\Mail;
+
+
+class ContactController extends Controller
+{
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('contact-us');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  App\Http\Requests\ContactRequest;
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactRequest $request)
+    {
+
+        $contactEmail = 'grojaslema@outlook.com'; //TODO: Set this value with Voyager Admin
+        try {
+            Mail::to($contactEmail)->send(new Contact($request));
+
+            return redirect()
+                ->route('contact-us.create')
+                ->with('success', __('Email sent successfully'));
+
+        } catch (\Exception $exception) {
+            $contactMessage          = new Contactmessage();
+            $contactMessage->name    = $request->name;
+            $contactMessage->email   = $request->email;
+            $contactMessage->phone   = $request->phone;
+            $contactMessage->subject = $request->subject;
+            $contactMessage->message = $request->message;
+            $contactMessage->save();
+
+            return redirect()
+                ->route('contact-us.create')
+                ->with('success', __('We will contact you as soon as possible'));
+        }
+
+        return redirect()
+            ->route('contact-us.create')
+            ->with('error', __('Something went wrong'));
+
+    }
+}
