8c78f5baf9d916021cdc527ea54bebf89d25f351
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:52:29 -0400

    Create headers.blade.php

---
+++ resources/views/common/headers.blade.php
@@ -0,0 +1,36 @@
+<!-- META -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="" />
+<meta name="author" content="" />
+<meta name="robots" content="" />
+<meta name="description" content="AutoCare is well designed creating websites of automotive repair shops, stores with spare parts and accessories for car repairs, car washes, car danting and panting, service stations, car showrooms painting, major auto centers and other sites related to cars and car services." />
+<meta property="og:title" content="Auto Care - Car Services Template" />
+<meta property="og:description" content="AutoCare is well designed creating websites of automotive repair shops, stores with spare parts and accessories for car repairs, car washes, car danting and panting, service stations, car showrooms painting, major auto centers and other sites related to cars and car services." />
+<meta property="og:image" content="http://autocare.dexignlab.com/xhtml/social-image.png" />
+<meta name="format-detection" content="telephone=no">
+
+
+<!-- FAVICONS ICON -->
+<link rel="icon" href="https://autocare.dexignlab.com/xhtml/images/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" type="image/x-icon" href="https://autocare.dexignlab.com/xhtml/images/favicon.png" />
+
+
+<!-- MOBILE SPECIFIC -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--[if lt IE 9]>
+<script src="https://autocare.dexignlab.com/xhtml/js/html5shiv.min.js"></script>
+<script src="https://autocare.dexignlab.com/xhtml/js/respond.min.js"></script>
+<![endif]-->
+
+<!-- load bootstrap from a cdn -->
+{{--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">--}}
+
+{{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>--}}
+
+
+<!-- STYLESHEETS -->
+<link rel="stylesheet" type="text/css" href="https://autocare.dexignlab.com/xhtml/css/style.min.css">
+<link rel="stylesheet" type="text/css" href="https://autocare.dexignlab.com/xhtml/css/templete.min.css">
+<link class="skin"  rel="stylesheet" type="text/css" href="https://autocare.dexignlab.com/xhtml/css/skin/skin-1.css">
