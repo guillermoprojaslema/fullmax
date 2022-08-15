901d2bc7a303326fec1bb9350c53e767e04221a6
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:52:11 -0400

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="page-content">
+
+        <div class="dlab-bnr-inr overlay-black-middle" style="background-image:url('https://autocare.dexignlab.com/xhtml/images/main-slider/slide1.jpg');">
+            <div class="container">
+                <div class="dlab-bnr-inr-entry">
+                    <h1 class="text-white ">All Services</h1>
+                    <div class="dlab-separator bg-primary"></div>
+                    <p class="text-white max-w800">There are many variations of passages of Lorem Ipsum typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- About Company -->
+        <div class="section-full bg-white content-inner-1"
+             style="background-image: url('https://autocare.dexignlab.com/xhtml/images/bg-img.png'); background-repeat: repeat-x; background-position: left bottom -37px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 about-contant">
+                        <div class="m-b20">
+                            <h2 class="text-uppercase m-t0 m-b10">Car<span class="text-primary"> Polish</span></h2>
+                            <span class="text-secondry font-16">Innovation &amp; Adoption</span>
+                            <div class="clear"></div>
+                        </div>
+                        <p class="m-b30">Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la
+                            mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras
+                            aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem Ipsum,
+                            necesitás estar seguro de que no hay nada avergonzante escondido en el medio del texto.
+                            Todos los generadores de Lorem Ipsum que se encuentran en Internet tienden a repetir trozos
+                            predefinidos cuando sea necesario, haciendo a este el único generador verdadero (válido) en
+                            la Internet</p>
+                        <div class="row">
+                            <div class="col-md-4 col-lg-4 col-sm-4 m-b15">
+                                <div class="icon-bx-wraper bx-style-1 p-tb15 p-lr10 center">
+                                    <div class="icon-bx-sm radius bg-primary m-b5"><a href="#" class="icon-cell"><i
+                                                class="ti-user"></i></a></div>
+                                    <div class="icon-content">
+                                        <h2 class="text-primary m-t20 m-b10"><span class="counter">1800</span>+</h2>
+                                        <p>Engineers &amp; Workers</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-lg-4 col-sm-4 m-b15">
+                                <div class="icon-bx-wraper bx-style-1 p-tb15 p-lr10 center">
+                                    <div class="icon-bx-sm radius bg-primary m-b5"><a href="#" class="icon-cell"><i
+                                                class="ti-home"></i></a></div>
+                                    <div class="icon-content">
+                                        <h2 class="text-primary m-t20 m-b10"><span class="counter">620</span>+</h2>
+                                        <p>Factory In Worldwide</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-lg-4 col-sm-4 m-b15">
+                                <div class="icon-bx-wraper bx-style-1 p-tb15 p-lr10 center">
+                                    <div class="icon-bx-sm radius bg-primary m-b5"><a href="#" class="icon-cell"><i
+                                                class="ti-harddrives"></i></a></div>
+                                    <div class="icon-content">
+                                        <h2 class="text-primary m-t20 m-b10"><span class="counter">1527</span>+</h2>
+                                        <p>Projects Completed</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="m-b15">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de
+                            texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año
+                            1500.</p>
+                        <p class="m-b15">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de
+                            texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año
+                            1500.</p>
+                    </div>
+                    <div class="col-lg-5 about-img">
+                        <img src="https://autocare.dexignlab.com/xhtml/images/img2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About Company END -->
+
+    </div>
+
+@endsection
