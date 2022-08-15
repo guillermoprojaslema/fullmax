5f9abd6da7b0ef58622902148b2806393f589647
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:52:24 -0400

    Create js.blade.php

---
+++ resources/views/common/js.blade.php
@@ -0,0 +1,15 @@
+<script src="https://autocare.dexignlab.com/xhtml/js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/bootstrap/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
+<script src="https://autocare.dexignlab.com/xhtml/plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+<script src="https://autocare.dexignlab.com/xhtml/js/custom.min.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="https://autocare.dexignlab.com/xhtml/js/dz.carousel.min.js"></script><!-- SORTCODE FUCTIONS  -->
+
+<script src='https://www.google.com/recaptcha/api.js'></script> <!-- Google API For Recaptcha  -->
