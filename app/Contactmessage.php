e0596a4b1e07958852b374199cb238d89933ac9b
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:51:27 -0400

    Create Contactmessage.php

---
+++ app/Contactmessage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contactmessage extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'contactmessages';
+}
