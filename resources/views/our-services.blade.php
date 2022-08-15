c5b71c03e8ac4b26bdcc77d833bd97063f94a6a5
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-14 20:52:05 -0400

    Create our-services.blade.php

---
+++ resources/views/our-services.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="page-content">
+
+        <!-- OUR SERVICES -->
+        <div class="section-full bg-white content-inner">
+            <div class="container">
+                <div class="section-head text-center">
+                    <h2 class="text-uppercase"> Nuestros servicios</h2>
+                    <div class="dlab-separator-outer ">
+                        <div class="dlab-separator bg-secondry style-skew"></div>
+                    </div>
+                    <p>{{__('Check our services')}}</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="icon-bx-wraper center m-b40">
+                            <div class="icon-bx-sm bg-secondry m-b20"><span class="icon-cell"><i
+                                        class="ti-reload text-primary"></i></span></div>
+                            <div class="icon-content">
+                                <h5 class="dlab-tilte text-uppercase">AIR CONDITIONING</h5>
+                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.| </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="icon-bx-wraper center m-b40">
+                            <div class="icon-bx-sm bg-secondry m-b20"><span class="icon-cell"><i
+                                        class="ti-car text-primary"></i></span></div>
+                            <div class="icon-content">
+                                <h5 class="dlab-tilte text-uppercase">BRAKE REPAIR</h5>
+                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.|</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="icon-bx-wraper center m-b40">
+                            <div class="icon-bx-sm bg-secondry m-b20"><span class="icon-cell"><i
+                                        class="ti-thumb-up text-primary"></i></span></div>
+                            <div class="icon-content">
+                                <h5 class="dlab-tilte text-uppercase">LUBE, OIL AND FILTERS</h5>
+                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.|</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="icon-bx-wraper center m-b40">
+                            <div class="icon-bx-sm bg-secondry m-b20"><span class="icon-cell"><i
+                                        class="ti-cup text-primary"></i></span></div>
+                            <div class="icon-content">
+                                <h5 class="dlab-tilte text-uppercase">BELTS AND HOSES</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                                    euismod </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="icon-bx-wraper center m-b40">
+                            <div class="icon-bx-sm bg-secondry m-b20"><span class="icon-cell"><i
+                                        class="ti-settings text-primary"></i></span></div>
+                            <div class="icon-content">
+                                <h5 class="dlab-tilte text-uppercase">ENGINE DIAGNOSTICS</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                                    euismod </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="icon-bx-wraper center m-b10">
+                            <div class="icon-bx-sm bg-secondry m-b20"><span class="icon-cell"><i
+                                        class="ti-pie-chart text-primary"></i></span></div>
+                            <div class="icon-content">
+                                <h5 class="dlab-tilte text-uppercase">TIRE AND WHEEL SERVICES</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                                    euismod </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- OUR SERVICES END-->
+    </div>
+@endsection
