5f88fe97d77451ac6ae6fb10e7f51dee5136a714
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-16 22:36:15 -0400

    Update dimmer.php

--- vendor/tcg/voyager/publishable/lang/es/dimmer.php
+++ vendor/tcg/voyager/publishable/lang/es/dimmer.php
@@ -10,4 +10,16 @@ return [
     'user'           => 'Usuario|Usuarios',
     'user_link_text' => 'Ver todos los usuarios',
     'user_text'      => 'Tiene :count :string en su base de datos. Haga clic en el botón de abajo para ver todos los usuarios.',
+
+    'rrss'           => 'Red Social|Redes Sociales',
+    'rrss_link_text' => 'Ver todos las redes sociales',
+    'rrss_text'      => 'Tiene :count :string en su base de datos. Haga clic en el botón de abajo para ver todos las redes sociales.',
+
+    'mensajes'           => 'Mensaje|Mensajes',
+    'mensajes_link_text' => 'Ver todos los mensajes',
+    'mensajes_text'      => 'Tiene :count :string en su base de datos. Haga clic en el botón de abajo para ver todos los mensajes.',
+
+    'servicios'           => 'Servicio|Servicios',
+    'servicios_link_text' => 'Ver todos los servicios',
+    'servicios_text'      => 'Tiene :count :string en su base de datos. Haga clic en el botón de abajo para ver todos los servicios.',
 ];
