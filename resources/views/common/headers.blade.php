93c4f09389fa45d9808134fb27ff775be4fdceac
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-08-20 21:55:16 -0400

    Update headers.blade.php

--- resources/views/common/headers.blade.php
+++ resources/views/common/headers.blade.php
@@ -1,39 +1,44 @@
 <!-- META -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="keywords" content="" />
-<meta name="author" content="" />
-<meta name="robots" content="" />
-<meta name="description" content="{{setting('site.description')}}" />
-<meta property="og:title" content="{{setting('site.title')}}" />
-<meta property="og:description" content="{{setting('site.description')}}" />
-<meta property="og:image" content="http://autocare.dexignlab.com/xhtml/social-image.png" />
+<meta name="keywords" content=""/>
+<meta name="author" content=""/>
+<meta name="robots" content=""/>
+<meta name="description" content="{{setting('site.description')}}"/>
+<meta property="og:title" content="{{setting('site.title')}}"/>
+<meta property="og:description" content="{{setting('site.description')}}"/>
+<meta property="og:image" content="http://autocare.dexignlab.com/xhtml/social-image.png"/>
 <meta name="format-detection" content="telephone=no">
 
 
 <!-- FAVICONS ICON -->
-<link rel="icon" href="https://autocare.dexignlab.com/xhtml/images/favicon.ico" type="image/x-icon" />
-<link rel="shortcut icon" type="image/x-icon" href="https://autocare.dexignlab.com/xhtml/images/favicon.png" />
+<link rel="icon" href="https://autocare.dexignlab.com/xhtml/images/favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" type="image/x-icon" href="https://autocare.dexignlab.com/xhtml/images/favicon.png"/>
 
 
 <!-- MOBILE SPECIFIC -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!--[if lt IE 9]>
-<script src="https://autocare.dexignlab.com/xhtml/js/html5shiv.min.js"></script>
-<script src="https://autocare.dexignlab.com/xhtml/js/respond.min.js"></script>
+
+<script src="{{asset('js/html5shiv.min.js')}}"></script>
+<script src="{{asset('js/respond.min.js')}}"></script>
+
 <![endif]-->
 
 <!-- load bootstrap from a cdn -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
 
 <!-- STYLESHEETS -->
-<link rel="stylesheet" type="text/css" href="https://autocare.dexignlab.com/xhtml/css/style.min.css">
-<link rel="stylesheet" type="text/css" href="https://autocare.dexignlab.com/xhtml/css/templete.min.css">
-<link class="skin"  rel="stylesheet" type="text/css" href="https://autocare.dexignlab.com/xhtml/css/skin/skin-1.css">
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/templete.min.css')}}">
+<link class="skin" rel="stylesheet" type="text/css" href="{{asset('css/skin-1.css')}}">
 
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
