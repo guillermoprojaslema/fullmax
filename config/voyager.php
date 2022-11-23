af060808557acc34b0fc11e45436a13693fcf728
Guillermo Rojas <97183610+grojaslema@users.noreply.github.com>
2022-11-23 18:05:25 -0300

    Update voyager.php

--- config/voyager.php
+++ config/voyager.php
@@ -183,7 +183,7 @@ return [
     |
     */
 
-    'primary_color' => '#22A7F0',
+    'primary_color' => '#E6910A ',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
